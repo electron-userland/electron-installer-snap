1271858031074954884090e273df45f6319a2b42
Mark Lee <electronjs@lazymalevolence.com>
2017-09-29 11:48:12 -0400

    CI: fix typo

--- ci/script.sh
+++ ci/script.sh
@@ -1,2 +1,2 @@
 sudo docker build -f ci/Dockerfile . -t electron-installer-snap-ci
-sudo docker run -it electron-installer-snap-ci npm ci/docker.sh
+sudo docker run -it electron-installer-snap-ci ci/docker.sh
