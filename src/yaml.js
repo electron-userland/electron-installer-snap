fa51714bf9949d453f3c3d02bf3d859a8b38ab55
Mark Lee <electronjs@lazymalevolence.com>
2017-09-29 14:40:42 -0400

    Add support for adding features to the snap

--- src/yaml.js
+++ src/yaml.js
@@ -19,9 +19,31 @@ const debug = require('debug')('electron-installer-snap:yaml')
 const fs = require('fs-extra')
 const merge = require('lodash/merge')
 const path = require('path')
+const pull = require('lodash/pull')
 const yaml = require('js-yaml')
 
 const defaultArgsFromApp = require('./default_args')
+
+const FEATURES = {
+  audio: {
+    packages: ['libpulse0'],
+    plugs: ['pulseaudio']
+  },
+  alsa: {
+    packages: ['libasound2'],
+    plugs: ['alsa']
+  },
+  browserSandbox: {
+    transform: 'transformBrowserSandbox'
+  },
+  mpris: {
+    transform: 'transformMPRIS'
+  },
+  webgl: {
+    packages: ['libgl1-mesa-glx', 'libglu1-mesa'],
+    plugs: ['opengl']
+  }
+}
 
 /**
  * Blank lines need to be dots, like Debian.
@@ -40,6 +62,14 @@ class SnapcraftYAML {
       })
   }
 
+  get app () {
+    return this.data.apps[this.appName]
+  }
+
+  get parts () {
+    return this.data.parts[this.appName]
+  }
+
   renameSubtree (parentObject, fromKey, toKey) {
     parentObject[toKey] = parentObject[fromKey]
     delete parentObject[fromKey]
@@ -51,20 +81,97 @@ class SnapcraftYAML {
     }
   }
 
+  transformFeature (feature) {
+    let featureData = FEATURES[feature]
+    if (!featureData) {
+      return
+    }
+
+    if (feature === 'audio' && this.features.indexOf('alsa') >= 0) {
+      return
+    }
+
+    if (featureData.feature) {
+      featureData = FEATURES[featureData.feature]
+    }
+
+    if (featureData.transform) {
+      this[featureData.transform]()
+    } else {
+      if (featureData.packages) {
+        this.parts['stage-packages'].push.apply(featureData.packages)
+      }
+      if (featureData.plugs) {
+        this.app.plugs.push.apply(featureData.plugs)
+      }
+    }
+  }
+
+  transformFeatures () {
+    if (!this.features) {
+      return
+    }
+
+    for (const feature in this.features) {
+      this.transformFeature(feature)
+    }
+  }
+
+  transformBrowserSandbox () {
+    debug('Replacing brower-support plug with browser-sandbox')
+    pull(this.app.plugs, ['browser-support'])
+    this.app.plugs.push('browser-sandbox')
+    if (!this.data.plugs) {
+      this.data.plugs = {}
+    }
+    this.data.plugs['browser-sandbox'] = {
+      'allow-sandbox': true,
+      interface: 'browser-support'
+    }
+    console.warn('This setting will trigger a manual review in the Snap store.')
+  }
+
+  transformMPRIS () {
+    debug('Adding MPRIS feature')
+    const mprisKey = `${this.appName}-mpris`
+    if (!this.app.slots) {
+      this.app.slots = []
+    }
+    this.app.slots.push(mprisKey)
+    if (!this.data.slots) {
+      this.data.slots = {}
+    }
+    this.data.slots[mprisKey] = {
+      interface: 'mpris',
+      name: String(this.features.mpris)
+    }
+  }
+
+  transformParts (packageDir) {
+    this.parts.source = path.dirname(packageDir)
+    this.parts.organize = {}
+    this.parts.organize[path.basename(packageDir)] = this.data.name
+  }
+
   transform (packageDir, userSupplied) {
     return defaultArgsFromApp(packageDir)
       .then(packageJSONArgs => {
-        merge(this.data, packageJSONArgs, userSupplied)
-        this.renameSubtree(this.data.parts, 'electronApp', this.data.name)
-        this.renameSubtree(this.data.apps, 'electronApp', this.data.name)
+        this.userSupplied = merge({}, packageJSONArgs, userSupplied)
+        this.appName = this.userSupplied.name
+        this.features = merge({}, this.userSupplied.features || {})
+        delete this.userSupplied.features
+
+        merge(this.data, this.userSupplied)
+
+        this.renameSubtree(this.data.parts, 'electronApp', this.appName)
+        this.renameSubtree(this.data.apps, 'electronApp', this.appName)
         this.data.description = convertBlankLines(this.data.description)
         this.validateSummary()
-        this.data.apps[this.data.name].command = `desktop-launch '$SNAP/${this.data.name}/${this.data.productName}'`
-        const parts = this.data.parts[this.data.name]
-        parts.source = path.dirname(packageDir)
-        parts.organize = {}
-        parts.organize[path.basename(packageDir)] = this.data.name
+        this.app.command = `desktop-launch '$SNAP/${this.data.name}/${this.data.productName}'`
+        this.transformFeatures()
+        this.transformParts(packageDir)
 
+        delete this.data.features
         delete this.data.productName
 
         return this.data
