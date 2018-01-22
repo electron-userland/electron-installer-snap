d2c99b110dcbc46bde87b8d2e3e05a1017850895
Mark Lee <electronjs@lazymalevolence.com>
2018-01-21 19:15:45 -0800

    Add test for non-existent snapcraft

--- test/index.js
+++ test/index.js
@@ -5,6 +5,10 @@ const snap = require('../src')
 const test = require('ava')
 const tmp = require('tmp-promise')
 
+function assertThrows (t, options, messageRegex) {
+  return t.throws(snap(options)).catch(err => t.regex(err.message, messageRegex))
+}
+
 test.beforeEach(t => {
   t.context.tempDir = tmp.dirSync({ prefix: 'electron-installer-snap-' })
   process.chdir(t.context.tempDir.name)
@@ -16,7 +20,8 @@ test.afterEach.always(t => {
 
 test('missing configuration', t => t.throws(snap, 'Missing configuration'))
 
-test('packaged app not found', t =>
-  t.throws(snap({})).catch(err => t.regex(err.message, /^Could not find, read, or parse package\.json/))
-)
+test('packaged app not found', t => assertThrows(t, {}, /^Could not find, read, or parse package\.json/))
+
+test('cannot find custom snapcraft', t => assertThrows(t, {snapcraft: '/foo/bar/non-existent'}, /Cannot locate \/foo\/bar\/non-existent in your system/))
+
 test.serial('snapcraft fails', t => snap({src: path.join(__dirname, 'fixtures', 'package-stub')}))
