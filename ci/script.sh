4ebe51c48bb3cecb43098b80fbc053ead70ff27c
Mark Lee <electronjs@lazymalevolence.com>
2019-05-19 14:15:33 -0700

    chore: use node 8 in docker

--- ci/script.sh
+++ ci/script.sh
@@ -1,1 +1,1 @@
-sudo docker run --interactive --tty --volume $(pwd):/code malept/electron-forge-container:latest /code/ci/docker.sh
+sudo docker run --interactive --tty --volume $(pwd):/code malept/electron-forge-container:node-8 /code/ci/docker.sh
