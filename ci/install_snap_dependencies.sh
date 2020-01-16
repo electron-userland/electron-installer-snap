08c26dc1044cb6a241296a22eca1ac29f053ad1b
Mark Lee <electronjs@lazymalevolence.com>
2020-01-15 22:56:24 -0800

    chore: switch from Travis CI to GitHub Actions

---
+++ ci/install_snap_dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -xe
+
+mkdir -p fakesnap/snap
+cp ci/snapcraft.yaml fakesnap/snap/
+pushd fakesnap
+snapcraft pull desktop-gtk3 electron-deps
+sed -i -e s:desktop-gtk3:desktop-gtk2:g snap/snapcraft.yaml
+snapcraft pull desktop-gtk2
+popd
+rm -r fakesnap
